fffecd27b7d1d03967ea12bb84209ac709f85f58
ablease <alexblease@gmail.com>
2015-01-21 19:39:01 +0000

    Write test for when user is logged in, they should be able to join London MDT chat

--- features/home.feature
+++ features/home.feature
@@ -8,5 +8,10 @@ Feature: The homepage
       And I want to Sign Up
       When I press Sign Up
       Then I should be able to Sign Up
-
+    
+    Scenario: When a Doctor is logged in
+      Given I am on the homepage
+      And I want to join my MDT chat
+      When I press my MDT
+      Then I should see my MDT chat room
      
--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -9,3 +9,15 @@ end
 Then(/^I should be able to Sign Up$/) do
   visit('/sign_up')
 end
+
+Given(/^I want to join my MDT chat$/) do
+  expect(page).to have_content 'London MDT'
+end
+
+When(/^I press my MDT$/) do
+  click_link('London MDT')
+end
+
+Then(/^I should see my MDT chat room$/) do
+  expect(page).to have_content 'Welcome to London MDT chat'
+end
