5da105062e913a67ac9742067276ff6dff51db4d
ablease <alexblease@gmail.com>
2015-01-22 12:03:33 +0000

    Refactor user management into user management controller

--- lib/lightbox.rb
+++ lib/lightbox.rb
@@ -4,6 +4,7 @@ require 'bcrypt'
 require 'em-websocket'
 
 require_relative 'controllers/application'
+require_relative 'controllers/users'
 require_relative 'data_mapper_setup'
 
 class Lightbox < Sinatra::Base
@@ -13,46 +14,6 @@ class Lightbox < Sinatra::Base
   set :views, Proc.new { File.join(root, "/views") }
   use Rack::Flash
 
-
-#  get '/' do
-#    erb :index
-#  end
-
-    get '/sign_up' do
-      erb :sign_up
-    end
-
-    post '/sign_up' do
-      @new_user = User.create(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
-      if 	@new_user.save
-        session[:user_id] = @new_user.id
-        flash[:notice] = "Successfully signed up"
-        redirect '/'
-      else
-        flash[:notice] = "Your passwort doesn't match, please try again"
-        redirect '/sign_up'
-      end
-
-   end
-
-   get '/login' do
-      erb :login
-    end
-
-    post '/login' do
-      email, password = params[:email], params[:password]
-      user = User.authenticate(email, password)
-
-      if user
-        session[:user_id] = user.id
-        flash[:notice] = "Successfully logged in"
-        redirect to('/')
-      else
-        flash[:notice] = ["The email or password are incorrect"]
-        redirect to('/login')
-      end
-    end
-   
   get '/mdts/london_mdt' do
    erb :mdts
   end
