ae4e4077fe26f9efe1b10773ec1908c2f219fcfd
ablease <alexblease@gmail.com>
2015-01-22 11:16:11 +0000

    Setup up Eventmachine websockets, enable one way chat, to do, allow all users to see chat on same page

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rack-flash3'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt'
-gem 'em_websocket'
+gem 'em-websocket'
 
 group :development, :test do
   gem 'capybara'
--- lib/lightbox.rb
+++ lib/lightbox.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'rack-flash'
 require 'data_mapper'
 require 'bcrypt'
+require 'em-websocket'
 
 env = ENV['Rack_Env'] || 'development'
 
@@ -13,62 +14,85 @@ DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
-class Lightbox < Sinatra::Base
+EM.run do
+  class Lightbox < Sinatra::Base
 
-  enable :sessions
-  use Rack::Flash
+    enable :sessions
+    use Rack::Flash
 
-  set :views, Proc.new { File.join(root,"..", "views") }
+    set :views, Proc.new { File.join(root,"..", "views") }
 
-  get '/' do
-    erb :index
-  end
-
-  get '/sign_up' do
-    erb :sign_up
-  end
-
-  post '/sign_up' do
-    @new_user = User.create(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
-    if 	@new_user.save
-      session[:user_id] = @new_user.id
-      flash[:notice] = "Successfully signed up"
-      redirect '/'
-    else
-      flash[:notice] = "Your passwort doesn't match, please try again"
-      redirect '/sign_up'
+    get '/' do
+      erb :index
     end
 
-  end
-
-  get '/login' do
-    erb :login
-  end
-
-  post '/login' do
-    email, password = params[:email], params[:password]
-    user = User.authenticate(email, password)
-
-    if user
-      session[:user_id] = user.id
-      flash[:notice] = "Successfully logged in"
-      redirect to('/')
-    else
-      flash[:notice] = ["The email or password are incorrect"]
-      redirect to('/login')
+    get '/sign_up' do
+      erb :sign_up
     end
-  end
-  
-  get '/mdts/london_mdt' do
-   erb :mdts
-  end
 
-  def current_user
-    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    post '/sign_up' do
+      @new_user = User.create(name: params["name"], email: params["email"], password: params["password"], password_confirmation: params["password_confirmation"])
+      if 	@new_user.save
+        session[:user_id] = @new_user.id
+        flash[:notice] = "Successfully signed up"
+        redirect '/'
+      else
+        flash[:notice] = "Your passwort doesn't match, please try again"
+        redirect '/sign_up'
+      end
+
+    end
+
+    get '/login' do
+      erb :login
+    end
+
+    post '/login' do
+      email, password = params[:email], params[:password]
+      user = User.authenticate(email, password)
+
+      if user
+        session[:user_id] = user.id
+        flash[:notice] = "Successfully logged in"
+        redirect to('/')
+      else
+        flash[:notice] = ["The email or password are incorrect"]
+        redirect to('/login')
+      end
+    end
+    
+    get '/mdts/london_mdt' do
+     erb :mdts
+    end
+
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
+    
+    #Websockets server logic goes here
+
+    @clients = []
+
+    EM::WebSocket.start(:host => '0.0.0.0', port => '3001') do |ws|
+      ws.onopen do |handshake|
+        @clients << ws
+        ws.send "Connected to #{handshake.path}."
+      end
+
+       ws.onclose do
+         ws.send "Closed."
+         @clients.delete ws
+       end
+
+       ws.onmessage do |msg|
+         puts "Received Message: #{msg}"
+         @clients.each do |socket|
+           socket.send msg
+         end
+       end
   end
-  
-  #Websockets server logic goes here
-  
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+     
+    # start the server if ruby file executed directly
+    run! if app_file == $0
+  end
 end
