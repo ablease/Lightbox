cb2a8a6e441bb70bd27fa776252c003226a9bda7
ablease <alexblease@gmail.com>
2015-01-22 14:26:51 +0000

    Remove commented out methods in lightbox.rb from previous refactor session

--- lib/controllers/users.rb
+++ lib/controllers/users.rb
@@ -37,5 +37,10 @@ class Lightbox < Sinatra::Base
       redirect to('/login')
     end
   end
+
+  def current_user
+    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  end
+
 end
 
--- lib/lightbox.rb
+++ lib/lightbox.rb
@@ -18,10 +18,6 @@ class Lightbox < Sinatra::Base
    erb :chat_rooms
   end
 
-  def current_user
-    @current_user ||= User.get(session[:user_id]) if session[:user_id]
-  end
-     
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
