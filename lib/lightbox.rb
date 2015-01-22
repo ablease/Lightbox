ce681d962bc01b6e981e7668fe661cde187b7269
ablease <alexblease@gmail.com>
2015-01-22 12:05:13 +0000

    Fix indentation in user controller

--- lib/lightbox.rb
+++ lib/lightbox.rb
@@ -21,9 +21,7 @@ class Lightbox < Sinatra::Base
   def current_user
     @current_user ||= User.get(session[:user_id]) if session[:user_id]
   end
-    
-
      
-    # start the server if ruby file executed directly
-    run! if app_file == $0
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
