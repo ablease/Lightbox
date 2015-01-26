ae5496b057b2a99380c85004151d28750648f7ee
ablease <alexblease@gmail.com>
2015-01-26 11:46:45 +0000

    Enable js.erb websockets include on document ready jquery function. Add jquery listeners to html form on chat page

--- lib/lightbox.rb
+++ lib/lightbox.rb
@@ -1,12 +1,10 @@
 require 'sinatra/base'
-require 'sinatra/rocketio'
 require 'rack-flash'
 require 'bcrypt'
-require 'em-websocket'
 
 require_relative 'controllers/application'
 require_relative 'controllers/users'
-require_relative 'controllers/sockets'
+#require_relative 'controllers/sockets'
 require_relative 'controllers/rooms'
 require_relative 'data_mapper_setup'
 
@@ -19,6 +17,8 @@ class Lightbox < Sinatra::Base
   use Rack::Flash
   use Rack::MethodOverride
 
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
