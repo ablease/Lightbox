69bd22d0d9609481a58a8173433b51ea05e32104
ablease <alexblease@gmail.com>
2015-02-06 15:27:19 +0000

    Begin Preek!

--- lib/controllers/helpers.rb
+++ lib/controllers/helpers.rb
@@ -1,5 +1,5 @@
 def current_user
-  @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  @current_user ||= User.get(session[:user_id])
 end
 
 def create_user
--- middleware/chat_backend.rb
+++ middleware/chat_backend.rb
@@ -17,17 +17,14 @@ module Chat
       if Faye::WebSocket.websocket?(env)
         ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
         ws.on :open do |event|
-          p [:open, ws.object_id]
           @clients << ws
         end
 
         ws.on :message do |event|
-          p [:message, event.data]
            @clients.each {|client| client.send(event.data) }
         end
 
         ws.on :close do |event|
-          p [:close, ws.object_id, event.code, event.reason]
           @clients.delete(ws)
           ws = nil
         end
